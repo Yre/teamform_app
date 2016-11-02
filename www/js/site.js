b28f71e148cb3cc24e8d0007b444af3c08134080
Mark CAI <hcaiaa@connect.ust.hk>
2016-11-03 02:54:26 +0800

    Add files via upload

---
+++ www/js/site.js
@@ -0,0 +1,14 @@
+function initalizeFirebase() {
+	
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCdq3FVBy0zM8nAb5olfoLfevUWOfBcztI",
+    authDomain: "lab-1dd51.firebaseapp.com",
+    databaseURL: "https://lab-1dd51.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "743797076627"
+  };
+  firebase.initializeApp(config);
+
+}    
+
